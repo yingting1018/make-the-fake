e111eee9102b8123ab2cc660164faab786a86bcb
vekim00100 <vekim@ucsc.edu>
2024-02-29 16:58:22 -0800

    added title background

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -4,6 +4,12 @@ class Title extends Phaser.Scene
     {
         super("titleScene")
     }
+
+    preload()
+    {
+        this.load.image('titlebg', './assets/img/titlebg.png');
+    }
+
     create()
     {
         let menuConfig = {
@@ -11,6 +17,7 @@ class Title extends Phaser.Scene
             fontSize: '28px',
             backgroundColor: '#F3B141'
         }
+        this.sprite = this.add.tileSprite(0, 0, 1100, 980, 'titlebg').setOrigin(0, 0);
         this.add.text(game.config.width/2, game.config.height/2 - borderUIsize - borderPadding, 'woof', menuConfig).setOrigin(0.5);
 
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
